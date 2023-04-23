5243ca42f644c1c38e71c6268fc2b026f0701234
Ted Tran <tedtran2019@gmail.com>
2023-04-22 19:28:27 -0700

    Okay, I think that's enough playing around. I know exactly what I want to do now, so let's just put all of this into a Jupyter Notebook in order and call it a day

--- test.py
+++ test.py
@@ -13,44 +13,24 @@ import pandas as pd
 # some features were scaled, and others removed entirely
 housing = fetch_california_housing(as_frame=True).frame
 
-# %%
-# Simple single feature linear regression example
-def single_feature_linear_regression(housing):
-    # Decide features and target
-    features = housing[['MedInc']]
-    target = housing[['MedHouseVal']]
-    # Split the data into training and test sets
-    features_train, features_test, target_train, target_test = model_selection.train_test_split(
-        features, target, test_size=0.3, random_state=964)
-    lin_model = linear_model.LinearRegression()
-    lin_model.fit(features_train, target_train)
-    target_prediction = lin_model.predict(features_test)
-    lin_RMSE = metrics.mean_squared_error(
-        target_test, target_prediction, squared=False)
-    print(lin_RMSE * 100000) # Roughly 83k
-
-single_feature_linear_regression(housing)
-  
-# %%
-# Random Forest Regression with a little more complexity
-def random_forest_regression(features, target, random_forest_model=RandomForestRegressor()):
-    features_train, features_test, target_train, target_test = model_selection.train_test_split(
-        features, target, test_size=0.3, random_state=964)
-    random_forest_model.fit(features_train, target_train)
-    importances = random_forest_model.feature_importances_
-    importances = pd.DataFrame({'feature': features.columns, 'importance': np.round(importances, 3)})
-    print(importances.sort_values('importance', ascending=False).set_index('feature'))
-    target_prediction = random_forest_model.predict(features_test)
-    rf_RMSE = metrics.mean_squared_error(
-        target_test, target_prediction, squared=False)
-    comparison = pd.DataFrame({'Actual': target_test.values.flatten(), 'Predicted': target_prediction.flatten()})
-    print(comparison)
-    print(rf_RMSE * 100000) # Roughly 51k
-
 # All features have an importance score of more than .01, so I won't drop any
 features = housing.drop('MedHouseVal', axis=1)
 target = housing[['MedHouseVal']]
-random_forest_regression(features, target)
+
+def regression(features, target, model):
+    features_train, features_test, target_train, target_test = model_selection.train_test_split(
+        features, target, test_size=0.3, random_state=964)
+    model.fit(features_train, target_train)
+    # Importances can be plotted with a bar chart
+    # importances = model.feature_importances_
+    # importances = pd.DataFrame({'feature': features.columns, 'importance': np.round(importances, 3)})
+    # print(importances.sort_values('importance', ascending=False).set_index('feature'))
+    target_prediction = model.predict(features_test)
+    model_RMSE = metrics.mean_squared_error(
+        target_test, target_prediction, squared=False)
+    # comparison = pd.DataFrame({'Actual': target_test.values.flatten(), 'Predicted': target_prediction.flatten()})
+    # print(comparison)
+    print(model_RMSE * 100000)
 
 # %% 
 print(fetch_california_housing(as_frame=True).DESCR)
@@ -70,22 +50,25 @@ print(correlation_matrix['MedHouseVal'].sort_values(ascending=False))
 # %%
 # Fine tuning a model? Use GridSearchCV or RandomizedSearchCV
 # Grid ensures optimal, but can take forever. Randomized is faster, but not optimal
-def grid_search(features, target):
+def grid_search(features, target, model, param_grid):
     features_train, features_test, target_train, target_test = model_selection.train_test_split(
         features, target, test_size=0.3, random_state=964)
-    random_forest_model = RandomForestRegressor()
-    param_grid = [
-        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
-        {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
-    ]
-    grid_search = model_selection.GridSearchCV(random_forest_model, param_grid, cv=5, scoring='neg_mean_squared_error')
+    grid_search = model_selection.GridSearchCV(model, param_grid, cv=5, scoring='neg_mean_squared_error')
     grid_search.fit(features_train, target_train)
     print(grid_search.best_params_)
     print(np.sqrt(-grid_search.best_score_)) # RMSE
     return grid_search.best_estimator_
 
-tuned_random_forest = grid_search(features, target) # max_features: 2 and n_estimators: 30 is optimal
 # %%
-random_forest_regression(features, target, tuned_random_forest) # Consistently silghtly better, RMSE of 50k
+param_grid = [
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]}, 
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
+]
+tuned_random_forest = grid_search(features, target, RandomForestRegressor(), param_grid)  # max_features: 2 and n_estimators: 30 is optimal
 
 # %%
+regression(housing[['MedInc']], target, RandomForestRegressor()) # RMSE is 97k
+regression(features, target, RandomForestRegressor()) # Untuned, 51k
+regression(features, target, tuned_random_forest) # Consistently better, 50k
+regression(housing[['MedInc']], target, linear_model.LinearRegression()) # 83k
+regression(features, target, linear_model.LinearRegression()) # 72k
